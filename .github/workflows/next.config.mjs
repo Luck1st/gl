a935b834db316ef7af76887b30a01123a16e5bd1
Luck1st <93330539+Luck1st@users.noreply.github.com>
2025-06-28 01:13:19 +0530

    Delete .github/workflows directory

--- .github/workflows/next.config.mjs
+++
@@ -1,8 +0,0 @@
-const isProd = process.env.NODE_ENV === 'production';
-
-export default {
-  output: 'export',
-  basePath: isProd ? '/lakshmiresume' : '',
-  assetPrefix: isProd ? 'https://Luck1st.github.io/lakshmiresume/' : '',
-};
-
